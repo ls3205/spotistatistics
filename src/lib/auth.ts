04979ad7d6a25ebb0589f144a297d4e350fedec7
ls3205 <ls3205@trevor.org>
2023-09-27 14:20:22 -0400

    started working on displaying player data

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,7 +16,7 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.SPOTIFY_CLIENT_ID!,
             clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
             authorization: 'https://accounts.spotify.com/authorize?' + new URLSearchParams({
-                scope: 'user-read-private user-read-email user-top-read user-read-recently-played',
+                scope: 'user-read-private user-read-email user-top-read user-read-recently-played user-read-playback-state',
             }),
         })
     ],
