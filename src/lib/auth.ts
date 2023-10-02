e4132c79a14f48d19b139d88f599c46db23452b6
ls3205 <ls3205@trevor.org>
2023-10-02 08:41:09 -0400

    hopefully fixed token refresh flow, I don't really have a way to check until my token runs out

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -53,7 +53,7 @@ export const authOptions: NextAuthOptions = {
                             "Content-Type": "application/x-www-form-urlencoded",
                             Authorization: 'Basic ' + (Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64'))
                         },
-                        body: {
+                        params: {
                             grant_type: "refresh_token",
                             refresh_token: token.refreshToken
                         },
