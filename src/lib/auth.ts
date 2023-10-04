0b44c4fcb07ebb57f37244b1de6f3fa8e2998429
ls3205 <ls3205@trevor.org>
2023-10-03 20:31:52 -0400

    new auth flow idea

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -48,14 +48,10 @@ export const authOptions: NextAuthOptions = {
                 return token
             } else {
                 try {
-                    const { data } = await axios.post(`https://accounts.spotify.com/api/token`, {
+                    const { data } = await axios.post(`https://accounts.spotify.com/api/token?grant_type=refresh_token&refresh_token=${token.refreshToken}`, {
                         headers: {
                             "Content-Type": "application/x-www-form-urlencoded",
                             Authorization: 'Basic ' + (Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64'))
-                        },
-                        params: {
-                            grant_type: "refresh_token",
-                            refresh_token: token.refreshToken
                         },
                         json: true
                     })
